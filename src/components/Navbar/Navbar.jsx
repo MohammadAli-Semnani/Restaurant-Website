c73b4df3a8646ea4e619523e0eb19b58e029243c
MohammadAli Semnani <brutalpc78@gmail.com>
2023-08-14 19:10:28 +0330

    create structure of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
-import { GiHamburgerMenu } from "react-icon/gi"
-import { MdOutlineRestaurantMenu } from "react-icon/md"
+// import { GiHamburgerMenu } from "react-icon/gi"
+// import { MdOutlineRestaurantMenu } from "react-icon/md"
+
+import images from "../../constants/images"
+import "./Navbar.css"
 
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='app__navbar'>
+
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt='logo' />
+      </div>
+
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#'>Home</a></li>
+        <li className='p__opensans'><a href='#'>About</a></li>
+        <li className='p__opensans'><a href='#'>Menu</a></li>
+        <li className='p__opensans'><a href='#'>Awards</a></li>
+        <li className='p__opensans'><a href='#'>Contact</a></li>
+      </ul>
       
     </nav>
   );
