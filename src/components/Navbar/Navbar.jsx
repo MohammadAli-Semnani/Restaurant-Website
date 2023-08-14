8fbf35ebf89042af97d271edfd8d0dce3027fd13
MohammadAli Semnani <brutalpc78@gmail.com>
2023-08-14 21:12:06 +0330

    create structure of navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { GiHamburgerMenu } from "react-icon/gi"
-// import { MdOutlineRestaurantMenu } from "react-icon/md"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { MdOutlineRestaurantMenu } from "react-icons/md"
 
 import images from "../../constants/images"
 import "./Navbar.css"
 
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
+
+
   return (
     <nav className='app__navbar'>
 
@@ -28,6 +32,20 @@ const Navbar = () => {
         <a href='/' className='p__opensans'> Book Table</a>
       </div>
       
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={()=>{setToggle(true)}} cursor="pointer"/>
+        <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+          <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{setToggle(false)}}/>
+          <ul className='app__navbar-smallscreen-links'>
+            <li className='p__opensans'><a href='#'>Home</a></li>
+            <li className='p__opensans'><a href='#'>About</a></li>
+            <li className='p__opensans'><a href='#'>Menu</a></li>
+            <li className='p__opensans'><a href='#'>Awards</a></li>
+            <li className='p__opensans'><a href='#'>Contact</a></li>
+          </ul>
+        </div>
+      </div>
+
     </nav>
   );
 };
